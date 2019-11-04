2aad87d1ec7b78e0d05e63de0483b3dcf005aaa5
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-04 22:28:48 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 # Add your variables here
 first_number=0
-second_number=log10
+second_number=1
+sum=first_number+second_number
+product=first_number*second_number
+quotient=first_number/second_number
